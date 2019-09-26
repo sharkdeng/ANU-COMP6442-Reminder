8dfb94fec6813f4eb91c289a52cd82f48e553ec2
Supriya <u6734521@anu.edu.au>
2019-09-26 22:51:23 +1000

    delete feature added with permanent data storage

--- app/src/main/java/com/anu/dolist/EditorActivity.java
+++ app/src/main/java/com/anu/dolist/EditorActivity.java
@@ -2,11 +2,15 @@ package com.anu.dolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+
+import java.util.HashSet;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -15,6 +19,13 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        /*
+        @author: u6734521
+        get the intent id from MainActivity and put it here
+        extra caution of -1 is put, to avoid getting wrong id
+
+         */
 
         EditText editText = findViewById(R.id.editText);
 
@@ -29,6 +40,11 @@ public class EditorActivity extends AppCompatActivity {
             MainActivity.arrayAdapter.notifyDataSetChanged();
         }
 
+        /*
+        @author: u6734521
+        When the text is changed save the changes.
+         */
+
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -39,6 +55,12 @@ public class EditorActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 MainActivity.list.set(noteId,String.valueOf(charSequence));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.anu.dolist", Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet(MainActivity.list);
+                sharedPreferences.edit().putStringSet("notes",set).apply();
+
 
             }
 
--- app/src/main/java/com/anu/dolist/MainActivity.java
+++ app/src/main/java/com/anu/dolist/MainActivity.java
@@ -1,9 +1,13 @@
 package com.anu.dolist;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,12 +18,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> list = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
-
+    /*
+    @author: u6734521
+     */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,17 +52,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        @author: u6734521
+        listView for notes taking with an example note
+        SharedPreferences to store data
+         */
+
         ListView listView = findViewById(R.id.listView);
-        list.add("Example note");
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.anu.dolist", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes",null);
+        if(set == null){
+            list.add("Example note");
+        }else{
+            list = new ArrayList(set);
+
+        }
+
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(arrayAdapter);
-
+        /*
+        @author: u6734521
+         to jump to editor activity when the list item is pressed.
+         */
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
                 intent.putExtra("noteId",i);
                 startActivity(intent);
+            }
+        });
+
+        /*
+        @author: u6734521
+        when the list is long pressed, pop up delete alert confirmation.
+        if opted yes, delete else keep data as such.
+        sharedPreference to update delete
+         */
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int itemDelete =i;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Delete Note")
+                        .setTitle("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                list.remove(itemDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.anu.dolist", Context.MODE_PRIVATE);
+
+                                HashSet<String> set = new HashSet(MainActivity.list);
+                                sharedPreferences.edit().putStringSet("notes",set).apply();
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+                return true;
             }
         });
     }
